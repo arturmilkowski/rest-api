7d4d57cf95333f72124f3f808da2815e9647099e
Artur Miłkowski <artur-milkowski@tlen.pl>
2021-10-13 10:13:24 +0200

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\{UserController, BookController, UserBookController};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::apiResource('users', UserController::class);
 
+Route::get('books/search', [BookController::class, 'search'])
+    ->name('books.search');
+Route::apiResource('books', BookController::class);
+Route::get('/users/{user}/books', [UserBookController::class, 'index'])
+    ->name('users.books.index');
+Route::get(
+    '/users/{user}/books/{book}/attach', [UserBookController::class, 'attach']
+)
+->name('users.books.attach');
+Route::get(
+    '/users/{user}/books/{book}/detach', [UserBookController::class, 'detach']
+)
+->name('users.books.detach');
+
+/*
 Route::middleware(['auth:sanctum'])->group(
     function () {
         Route::apiResource('users', UserController::class);
@@ -28,3 +34,4 @@ Route::middleware(['auth:sanctum'])->group(
             ->name('users.books.detach');
     }
 );
+*/
